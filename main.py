cafaff97e662f002ff8abc469cf2d63baaba5e5a
Pierre <uitxj@student.kit.edu>
2024-12-03 16:47:48 +0100

    implemented admin features

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import uvicorn
 from loguru import logger
 import redis.asyncio as redis
 from fastapi_limiter import FastAPILimiter
-from fastapi_limiter.depends import RateLimiter
 from fastapi import FastAPI
 
 from routers.auth import auth
 from routers.slice import slice
+from routers.admin import admin
 from dependencies import get_config, get_client, get_slice_data_base
+
 
 async def lifespan(_: FastAPI):
     logger.remove(0)
@@ -17,23 +18,31 @@ async def lifespan(_: FastAPI):
     if not client:
         logger.error(f"Unable to connect via gRPC shutting down API")
         sys.exit(1)
-    
+
     max_slices = client.size_slice_ident()
     logger.info(f"Identified MAX_SLICES as {max_slices}")
     slice_database = get_slice_data_base()
     slice_database += [None for _ in range(max_slices)]
 
     logger.info("Configuring Rate Limiter")
-    redis_connection = redis.Redis(host=config.redis_url, port=6379, password=config.redis_password.get_secret_value(), encoding="utf8")
+    redis_connection = redis.Redis(
+        host=config.redis_url,
+        port=6379,
+        password=config.redis_password.get_secret_value(),
+        encoding="utf8",
+    )
     await FastAPILimiter.init(redis=redis_connection)
     logger.info("Startup Complete!")
     yield
     await FastAPILimiter.close()
 
+
 config = get_config()
 app = FastAPI(lifespan=lifespan)
 app.include_router(auth)
 app.include_router(slice)
+app.include_router(admin)
+
 
 @app.get("/")
 def read_root():
@@ -41,6 +50,14 @@ def read_root():
         "message": "Welcome to P4Slice your framework for creating slice instances!"
     }
 
+
 if __name__ == "__main__":
     # TODO: for production set reload to false
-    uvicorn.run('main:app', host=config.server_url, port=config.server_port, log_level='info', workers=4, reload=True)
+    uvicorn.run(
+        "main:app",
+        host=config.server_url,
+        port=config.server_port,
+        log_level="info",
+        workers=4,
+        reload=True,
+    )
--- routers/admin.py
+++ routers/admin.py
@@ -1,11 +1,38 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi_limiter.depends import RateLimiter
+from typing import Annotated, List
 
-from dependencies import get_config
+from models import FirewallEntry, User
+from internal.authlib import current_user_is_admin
+from dependencies import get_config, get_client
 
 config = get_config()
-admin = APIRouter(dependencies=[Depends(RateLimiter(times=config.rate_limit_per_minute, minutes=1))])
+admin = APIRouter(
+    prefix="/admin",
+    dependencies=[
+        Depends(RateLimiter(times=config.rate_limit_per_minute, minutes=1)),
+        Depends(current_user_is_admin),
+    ],
+)
 
-@admin.get("/monitor")
+
+@admin.get("/monitor", response_model=List)
 def monitor():
-    return {"message": "test"}
+    client = get_client()
+    base_model = client.bfrt_info.learn_get("digest_inst")
+    probe = client.loop_digest(base_model)
+    if not probe:
+        probe.append("No digests received!")
+    return probe
+
+
+@admin.get("/table_info", response_model=List)
+def table_info():
+    client = get_client()
+    return client.get_base_info()
+
+
+@admin.post("/firewall")
+def add_firewall(entry: FirewallEntry):
+    client = get_client()
+    client.add_firewall_entry(entry.src_addr, entry.prefix_len)
